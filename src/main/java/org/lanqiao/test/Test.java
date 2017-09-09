9438fafcb45d9dc2b40d2bfe36d0155310dcac36
liuan <526004095@qq.com>
2017-09-09 17:11:10 +0800

    Update Test.java

---
+++ src/main/java/org/lanqiao/test/Test.java
@@ -0,0 +1,14 @@
+package org.lanqiao.test;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by 刘安 on 2017/9/9.
+ */
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException{
+        System.out.println("Hello Word");
+
+    }
+}
